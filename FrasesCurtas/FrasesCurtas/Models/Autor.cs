57ac421dd413085d3a935c2a3e5629439c3d9129
Daniel Pelissari <dpelissari1@gmail.com>
2023-03-30 16:56:19 -0300

    Ajustes na validação de cadastro de autor e formatos de imagens aceitas

--- FrasesCurtas/FrasesCurtas/Models/Autor.cs
+++ FrasesCurtas/FrasesCurtas/Models/Autor.cs
@@ -6,16 +6,16 @@ namespace FrasesCurtas.Models
     {
         [Key]
         public Guid Id { get; set; }
-        [Required]
+        [Required(ErrorMessage = "O campo nome é obrigatório")]
         [MaxLength(250)]
         public string Nome { get; set; }
+
+        [Required(ErrorMessage = "O campo data de cadastro é obrigatório")]
         public DateTime DataCadastro { get; set; } = DateTime.Now;
         public bool Ativo { get; set; } = true;
 
         public string CaminhoImagem { get; set; }
-
         public ICollection<Frase> Frases { get; set; }
-
         // metodo para geração do id em Guid
         public void GerarNovoId()
         {
