137590836efc73b07ba7632dc58f6756ef14fd6e
Daniel Pelissari <dpelissari1@gmail.com>
2023-05-11 10:38:19 -0300

    Novo menu sidebar

--- FrasesCurtas/FrasesCurtas/wwwroot/script.js
+++ FrasesCurtas/FrasesCurtas/wwwroot/script.js
@@ -1,28 +1,68 @@
 ﻿/* FUNCAO PARA TORNAR O MENU FIXO NA PÁGINA */
-$(window).scroll(function () {
-    if (450 < $(this).scrollTop()) {
-        if (!$('header').hasClass('fixo'))
-            $('header').addClass('fixo');
-    } else {
-        if ($('header').hasClass('fixo'))
-            $('header').removeClass('fixo');
+//$(window).scroll(function () {
+//    if (450 < $(this).scrollTop()) {
+//        if (!$('header').hasClass('fixo'))
+//            $('header').addClass('fixo');
+//    } else {
+//        if ($('header').hasClass('fixo'))
+//            $('header').removeClass('fixo');
+//    }
+//});
+
+
+
+
+//$(document).ready(function () {
+//    const itemsMenu = document.querySelectorAll('.top-nav .parent-item');
+//    itemsMenu.forEach(parentMenu => {
+//        itemsMenu.addEventListener("click", toggleSubmenu);
+//    })
+//});
+
+
+//function toggleSubmenu() {
+//    this.classList.toggle('active');
+//}
+
+
+//window.fecharMenu = function () {
+//    alert('ok');
+//    var x = document.getElementById("toogle-button");
+//    console.log(x);
+//    x.cheched = true;
+//}
+
+
+
+//window.abreMenu = function () {
+//    const nav = document.querySelector(".nav-container");
+
+//    if (nav) {
+//        if (nav.classList.contains("is-active")) {
+//            nav.classList.remove("is-active");
+//        } else {
+//            nav.classList.add("is-active");
+//        }
+//    }
+//};
+
+
+
+window.abreMenu = function () {
+    $(".nav-container").toggleClass("is-active");
+};
+
+window.fecharMenu = function () {
+    const navContainer = document.querySelector('.nav-container');
+    navContainer.classList.remove('is-active');
+};
+
+window.addEventListener('click', function (event) {
+    const navContainer = document.querySelector('.nav-container');
+    const navItems = document.querySelector('.nav-items');
+    if (navContainer.contains(event.target) || navItems.contains(event.target)) {
+        return;
     }
+    fecharMenu();
 });
 
-/* ATIVA MENU MOBILE */
-window.AtivarMenuMblExt = function () {
-    var ativo = $('ul.navegar').hasClass('menuMobile');
-
-    if (ativo)
-        OcultarMenuMblExt();
-    else {
-        $('ul.navegar').addClass('menuMobile');
-        $('#btn-ext-mobile').attr('src', 'imagens/ico-fechar.svg');
-    }
-}
-
-/* OCULTA MENU MOBILE */
-window.OcultarMenuMblExt = function () {
-    $('ul.navegar').removeClass('menuMobile');
-    $('#btn-ext-mobile').attr('src', 'imagens/ico-menu-mobile.svg');
-}
