45245c3fb1d8d8e030e8e224141e4032477ed8b4
Daniel Pelissari <dpelissari1@gmail.com>
2023-06-28 14:31:27 -0300

    Validação do login e ajustes no menu

---
+++ FrasesCurtas/FrasesCurtas/Models/Login.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FrasesCurtas.Models {
+    public class Login {
+
+        [Required(ErrorMessage = "O campo login é obrigatório")]
+        public string Usuario { get; set; }
+
+        [Required(ErrorMessage = "O campo senha é obrigatório")]
+        public string Senha { get; set; }
+    }
+}
