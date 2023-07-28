3afd312cbf5d5d18e48a442cedeaf5b06b0256bb
Daniel Pelissari <dpelissari1@gmail.com>
2023-07-28 15:52:40 -0300

    Ajustes diversos

--- FrasesCurtas/FrasesCurtas/Models/CategoriaFrase.cs
+++ FrasesCurtas/FrasesCurtas/Models/CategoriaFrase.cs
@@ -5,17 +5,23 @@ namespace FrasesCurtas.Models {
         [Key]
         public Guid Id { get; set; }
         [MaxLength(250)]
+
+        [Required(ErrorMessage = "O campo categoria é obrigatório")]
         public string Categoria { get; set; }
 
-        [MaxLength(250)]
+        [Required(ErrorMessage = "O campo descrição é obrigatório")]
+        [MaxLength(500, ErrorMessage = "A descrição deve ter no máximo 500 caracteres")]
         public string Descricao { get; set; }
+   
 
         [Required(ErrorMessage = "O campo data de cadastro é obrigatório")]
         public DateTime DataCadastro { get; set; } = DateTime.Now;
 
         public ICollection<Frase> Frases { get; set; }
 
+        [Required(ErrorMessage = "O campo ativo é obrigatório")]
         public bool Ativo { get; set; } = true;
+
         public void GerarNovoId() {
             Id = Guid.NewGuid();
         }
--- FrasesCurtas/FrasesCurtas/Models/Frase.cs
+++ FrasesCurtas/FrasesCurtas/Models/Frase.cs
@@ -7,14 +7,20 @@ namespace FrasesCurtas.Models {
 
         public Guid IdAutor { get; set; }
 
+
+        [Required(ErrorMessage = "O campo categoria é obrigatório")]
         public Guid IdCategoriaFrase { get; set; }
 
+
+        [Required(ErrorMessage = "O campo autor é obrigatório")]
         public Autor Autor { get; set; }
 
+
+        [Required(ErrorMessage = "O campo categoria é obrigatório")]
         public CategoriaFrase Categoria { get; set; }
 
-        [Required]
-        [MaxLength(500)]
+
+        [Required(ErrorMessage = "O campo frase é obrigatório")]
         public string? Descricao { get; set; }
 
         public DateTime? DataCadastro { get; set; } = DateTime.Now;
