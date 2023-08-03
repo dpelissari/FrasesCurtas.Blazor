fcbffb71a71aade22080b2f13c902d71fccdbbee
Daniel Pelissari <dpelissari1@gmail.com>
2023-08-03 20:43:48 -0300

    Foto aleatória no outdoor com API di Unsplash

---
+++ FrasesCurtas/FrasesCurtas/Models/HttpClientExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.Json;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace FrasesCurtas.Models {
+    public static class HttpClientExtensions {
+        public static async Task<T> GetJsonAsync<T>(this HttpClient httpClient, string requestUri) {
+            var response = await httpClient.GetAsync(requestUri);
+            response.EnsureSuccessStatusCode();
+            var json = await response.Content.ReadAsStringAsync();
+            return JsonSerializer.Deserialize<T>(json, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+        }
+    }
+}
---
+++ FrasesCurtas/FrasesCurtas/Models/UnsplashApiResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace FrasesCurtas.Models {
+    public class UnsplashApiResponse {
+        public UnsplashApiUrls Urls { get; set; }
+    }
+
+    public class UnsplashApiUrls {
+        public string Regular { get; set; }
+    }
+}
